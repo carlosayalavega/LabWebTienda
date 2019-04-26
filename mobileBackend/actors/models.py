2f4f9096d626473bf400830a650c1599181cd590
carlosayalavega <carlos.ayala.vega.cav@gmail.com>
2019-04-25 23:24:51 -0500

    Add Files via upload

---
+++ mobileBackend/actors/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Actor(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    dp = models.CharField(max_length=512, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
